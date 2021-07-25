a42d403e7180d335c3e60bfa18e06b04c0fd6877
DouglasEde <briancarter340@gmail.com>
2021-07-25 00:22:57 -0700

    added new component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ExpenseItem = () => {
+    return (
+        <div>
+           <h2>Expense Item</h2>
+           <p>01/14/2021</p>
+           <p>Diner</p>
+           <p>$320</p>
+        </div>
+    )
+}

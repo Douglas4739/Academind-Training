7129f3fa1f88cfcffb36a5c0c92f3d78845e8bc0
DouglasEde <briancarter340@gmail.com>
2021-07-25 00:51:26 -0700

    made variables to replace hard coded data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './ExpenseItem.css';
 
 export const ExpenseItem = () => {
+    const expenseDate= new Date(2021, 0, 14);
+    const expenseTitle = 'Diner';
+    const expenseAmount = 320.64;
+
     return (
-      <div className="expense">
-        <div>01/14/2021</div>
-        <div>
-          <h2>Diner</h2>
-          <p>$320</p>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">{expenseAmount}</div>
         </div>
       </div>
     );

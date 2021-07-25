03d2e4602ee2afeb70590c2e2243079494de444a
DouglasEde <briancarter340@gmail.com>
2021-07-25 00:35:33 -0700

    added styling for Expense Item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
 
 export const ExpenseItem = () => {
     return (
+      <div className="expense">
+        <div>01/14/2021</div>
         <div>
-           <h2>Expense Item</h2>
-           <p>01/14/2021</p>
-           <p>Diner</p>
-           <p>$320</p>
+          <h2>Diner</h2>
+          <p>$320</p>
         </div>
-    )
+      </div>
+    );
 }

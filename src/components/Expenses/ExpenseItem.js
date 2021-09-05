50b37c4c8e7431f2464c520b3aa3e295bd20d8c2
DouglasEde <briancarter340@gmail.com>
2021-09-05 12:27:48 -0700

    added state hook to event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,15 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 export const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
 
-  let title = props.title;
 
   const clickHandler = () => {
-    title = 'Updated!';
+    setTitle('Updated!');
+    console.log(title);
   };
 
   return (

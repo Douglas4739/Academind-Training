27b7df082667a5ae6340756633c12bb0c5c04c50
DouglasEde <briancarter340@gmail.com>
2021-09-05 17:24:40 -0700

    fixed small bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -53,7 +53,7 @@ const ExpenseForm = (props) => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-
+    console.log(expenseData);
     props.onSaveExpenseData();
     setEnteredTitle("");
     setEnteredAmount("");

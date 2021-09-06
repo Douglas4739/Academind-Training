5df7a692eaf6a499c94d6ede5d65a22ad36bd30d
DouglasEde <briancarter340@gmail.com>
2021-09-05 17:43:14 -0700

    fixed addExpenseHandler

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('in App.js');
+    console.log(expense);
+  }
+
+
   return (
     <div>
       <ExpenseList items={expenses} />
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
     </div>
   );
 }
